f76df1c6d1b68c1da4d37852c61cc5fa379d3dd4
Klinke <klinkwort92@gmail.com>
2020-04-22 15:36:59 +0200

    Add project files.

---
+++ Lights/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+
+namespace Lights.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public readonly string[] HEX =
+        { 
+            "#ff0000", 
+            "#ffa500", 
+            "#ffff00", 
+            "#008000",
+            "#0000ff",
+            "#4b0082",
+            "#ee82ee"
+        };
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ Lights/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function addBlinks(classToTarget, buttonID) {
+    var elementsWithClass = document.getElementsByClassName(classToTarget);
+    var button = document.getElementById(buttonID);
+
+    if (button.innerHTML != "STOP") {
+        button.innerHTML = "STOP";
+        for (var i = 0; i < elementsWithClass.length; i++) {
+            elementsWithClass[i].classList += " animated";
+            
+        }
+    } else {
+        button.innerHTML = "START";
+        for (var i = 0; i < elementsWithClass.length; i++) {
+            elementsWithClass[i].classList = "light-bulb";
+        }
+    }
+
+}
