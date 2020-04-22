854792bc5fdef0bc0056af240889d0ebfbfb40bf
Klinke <klinkwort92@gmail.com>
2020-04-22 20:58:49 +0200

    Added function to change size of the bulbs Changed variable names in start/stop function Styling to body and elements

--- Lights/wwwroot/js/site.js
+++ Lights/wwwroot/js/site.js
@@ -3,21 +3,38 @@
 
 // Write your Javascript code.
 
+
+// THE LIGHT START/STOP FUNCTION
 function addBlinks(classToTarget, buttonID) {
-    var elementsWithClass = document.getElementsByClassName(classToTarget);
+    var targetClass = document.getElementsByClassName(classToTarget);
     var button = document.getElementById(buttonID);
 
     if (button.innerHTML != "STOP") {
         button.innerHTML = "STOP";
-        for (var i = 0; i < elementsWithClass.length; i++) {
-            elementsWithClass[i].classList += " animated";
-            
+        for (var i = 0; i < targetClass.length; i++) {
+            targetClass[i].classList += " animated";
+
         }
     } else {
         button.innerHTML = "START";
-        for (var i = 0; i < elementsWithClass.length; i++) {
-            elementsWithClass[i].classList = "light-bulb";
+        for (var i = 0; i < targetClass.length; i++) {
+            targetClass[i].classList = "light-bulb";
         }
     }
 
 }
+
+// LIGHTS SIZE FUNCTION
+
+function changeSize(sizeValue, targetId) {
+    var bulbToChange = document.getElementById(targetId);
+    bulbToChange.style.width = sizeValue + "px";
+    bulbToChange.style.height = sizeValue + "px";
+    console.log(sizeValue);
+}
+
+
+//EVENTLISTENERS
+
+document.getElementById("button").addEventListener("click", function () { addBlinks("light-bulb", "button") });
+
